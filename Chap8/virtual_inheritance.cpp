63d31deed935994038a019d806a7ccc6346b104f
JuHan Park <iiiamst16@gmail.com>
2023-11-15 23:59:35 +0900

    virtual inheritance

---
+++ Chap8/virtual_inheritance.cpp
@@ -0,0 +1,31 @@
+class BaseIO
+{
+public:
+    int mode;
+};
+
+class In : virtual public BaseIO
+{
+public:
+    int readPos;
+};
+
+class Out : virtual public BaseIO
+{
+public:
+    int writePos;
+};
+
+class InOut : public In, public Out
+{
+public:
+    bool safe;
+};
+
+int main(){
+    InOut ioObj;
+
+    ioObj.readPos = 10;
+    ioObj.writePos = 10;
+    
+}
